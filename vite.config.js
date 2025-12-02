af1b9611581ca46f4e11cbfbd2af5551ae86da53
廖翊翔 <82229244+yixiang-liao@users.noreply.github.com>
2025-12-02 11:06:57 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/hengchun-sensibility/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: '恆春感性 Hengchun Sensibility',
+        short_name: 'IHL',
+        start_url: '/hengchun-sensibility/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './icon/icon_icon-192-1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './icon/icon_icon-512-1.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
