ae03448a41016b7ca785bcdd5c0fe43fc88d6590
廖翊翔 <82229244+yixiang-liao@users.noreply.github.com>
2025-12-08 00:15:53 +0800

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import BackToTopButton from "../components/BackToTopButton";
 import InnerPages from "../layouts/HomePage/InnerPages";
 
 const HomePage = () => {
+  const [showLetter, setShowLetter] = useState(false);
   return (
     <div className="homepage">
       <div className="home" id="home">
@@ -16,7 +17,17 @@ const HomePage = () => {
               alt="Hengchun Sensibility Logo"
               className="logo"
             />
-            <div className="line"></div>
+            {/* <div className="line"></div> */}
+            {/* <img
+            className="msg_in_bottle"
+              src="./img/homepage/msg_in_Bottle.png"
+            /> */}
+            <img
+              className="msg_in_bottle glow"
+              src="./img/homepage/msg_in_Bottle.png"
+              onClick={() => setShowLetter(true)}
+              style={{ cursor: "pointer" }}
+            />
             <p>在南國風與光裡，找到生活的節奏</p>
           </div>
         </div>
@@ -28,13 +39,29 @@ const HomePage = () => {
             });
           }}
         >
+          {/* <img
+            src="./img/homepage/web_arrow r.png"
+            alt="Illustration"
+            className="sun-illustration"
+          /> */}
           <img
-            src="./img/homepage/sun-2.png"
-            alt="Sun Illustration"
+            src="./img/homepage/web_arrow_w.png"
+            alt="Illustration"
             className="sun-illustration"
           />
         </div>
       </div>
+
+      {/* 📌 Popup 區塊 */}
+      {showLetter && (
+        <div className="popup-overlay" onClick={() => setShowLetter(false)}>
+          <img
+            className="popup-img"
+            src="./img/homepage/web_letter2.png"
+            alt="Letter"
+          />
+        </div>
+      )}
 
       <InnerPages />
 
